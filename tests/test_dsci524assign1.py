fbdda6af52dda0db890f1f4f5b1f4eef763c29a4
Justin Fu <jufu11@gmail.com>
2021-02-25 17:56:19 -0800

    added unit tests for catbind

--- tests/test_dsci524assign1.py
+++ tests/test_dsci524assign1.py
@@ -1,5 +1,15 @@
 from dsci524assign1 import __version__
 from dsci524assign1 import dsci524assign1
 
+import pandas as pd
+
+
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_catbind():
+    a = pd.Categorical(["character", "hits", "your", "eyeballs"])
+    b = pd.Categorical(["but", "integer", "where it", "counts"])
+    assert ((dsci524assign1.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
+    assert ((dsci524assign1.catbind(a, b)).categories == ["but", "character",
+            "counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
